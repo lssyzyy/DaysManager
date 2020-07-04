7a9b47ca1f19b146254d8b20344bccc96f64086b
郑逸旸 <1050295477@qq.com>
2020-07-04 10:26:35 +0800

    //菜单添加

--- app/src/main/java/com/example/mypersonalmanager/MainActivity.java
+++ app/src/main/java/com/example/mypersonalmanager/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.mypersonalmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.mnu_pay:
+                Log.i(TAG,"个人收支记账管理被点击");
+                break;
+            case R.id.mnu_days:
+                Log.i(TAG,"个人日程管理被点击");
+                break;
+            case R.id.mnu_exit:
+                break;
+        }
+        return true;
+        // return super.onPrepareOptionsMenu(item);
     }
 }
